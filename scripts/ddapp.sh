4b9327cf5d377a13bfdd5cdd336fc0a94d7a030b
Odysseas Lamtzidis <odyslam@gmail.com>
2021-09-18 13:52:07 +0300

    bind ethsign default dirs to container

--- scripts/ddapp.sh
+++ scripts/ddapp.sh
@@ -7,9 +7,20 @@
 # That directory is the WORKDIR that is defined in Dockerfile, thus the commands will be executed inside the container,
 # but in the workdir of the dev machine from where the `docker run` command was invoked.
 # This is what enables the container to be a drop-in replacement of the `dapp` command.
-# Read more about `docker run`: https://docs.docker.com/engine/reference/commandline/run/
+# Read more about `docker run`: https://docs.docsocker.com/engine/reference/commandline/run/
 
-docker run -it --env "$(env | grep -v 'NIX|TERM')" --name ddapptools --mount type="bind",source="$(pwd)",target=/dapptools odyslam/ddapptools:latest $@
+ BINDED_VOLUMES="--mount type=bind,source=$(pwd),target=/dapptools"
+if [[ -e ${ETH_KEYSTORE} ]];then
+  BINDED_VOLUMES="$BINDED_VOLUMES --mount type=bind,source='${ETH_KEYSTORE}',target=/.ethereum/keystore"
+else
+  if [[ -e $HOME/Library/Ethereum/keystore ]]; then
+    BINDED_VOLUMES="${BINDED_VOLUMES} --mount type=bind,source=$HOME/Library/Ethereum/keystore,target=/root/.ethereum/keystore"
+  elif [[ -e $HOME/.ethereum/keystore  ]]; then
+    BINDED_VOLUMES="${BINDED_VOLUMES} --mount type=bind,source=$HOME/library/ethereum/keystore,target=/root/.ethereum/keystore"
+  fi
+fi
+echo $BINDED_VOLUMES
+docker run -it --env "$(env | grep -ve 'HOME' -e 'NIX' -e 'TERM' -e 'SSH' -e 'PATH')" --name ddapptools $BINDED_VOLUMES odyslam/ddapptools:latest "$@"
 # Stop container and remove it. This is required so that we can `docker run` the same image but with a new argument (thus a new container)
 # output of the commands is silenced
 docker stop ddapptools > /dev/null
